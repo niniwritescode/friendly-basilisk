a2390b00aae16c0fafd3c74a94365332b4b3ef3f
niniwritescode <nandinibijnor146@gmail.com>
2025-08-25 16:25:38 +0530

    Added break and continue exercises

---
+++ beginner-python/mybrkncont.py
@@ -0,0 +1,21 @@
+# Break statement exercise
+for i in range(12):
+    if(i == 10):
+        print("loop broke")
+        break
+    print("5 X", i + 1 , "=", 5 * (i+1))
+       
+# Break statement exercise
+for i in range(12):
+    if(i == 10):
+        print("skip the iteration")
+        continue
+    print("5 X", i , "=", 5 * (i))
+       
+
+
+
+
+
+
+
--- beginner-python/mywhileloop.py
+++ beginner-python/mywhileloop.py
@@ -4,18 +4,24 @@ while(i<=38):
     i = int(input("Enter a number: "))
     print(i)
   
-
 print("Done with the loop")
+
 #PROGRAM 2
-# count = 5
-# while(count>0):
-#     print(count)
-#     count = count - 1
-# else:
-#     print("I am inside else")
+count = 5
+while(count>0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
 
-
-
+#PROGRAM 3 do-while loop emulation
+i=0
+while(True):
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break
+       
 
 
 
