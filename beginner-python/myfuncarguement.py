bf42a238303007c1c266c0349811cdc0a18fed2b
niniwritescode <nandinibijnor146@gmail.com>
2025-08-30 16:28:38 +0530

    "Added new function file for practice"

---
+++ beginner-python/myfuncarguement.py
@@ -0,0 +1,27 @@
+def average(a=9, b=1):
+    print("The average is", (a + b) / 2)
+
+# average(4, 6)
+average(b=9)
+average(a=21)
+
+def name(fname, mname ="John", lname = "Whatson"):
+    print("Hello,", fname, mname, lname)
+
+name("Amy", "Louise")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
