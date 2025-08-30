77d1ff7814a4c4b113a136dc65a062904b6e34ea
niniwritescode <nandinibijnor146@gmail.com>
2025-08-30 15:35:35 +0530

    Add myfunctions.py with basic mathematical functions

---
+++ beginner-python/myfunctions.py
@@ -0,0 +1,47 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isGreater(a, b):
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater or equal")  
+def isLesser(a, b):
+    if(a<b):
+        print("first number is lesser")
+    else:
+        print("second number is lesser or equal")
+
+
+a = 9
+b = 8
+isGreater(a, b)
+
+calculateGmean(a, b)
+
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c = 8
+d = 7
+isGreater(c, d)
+
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
