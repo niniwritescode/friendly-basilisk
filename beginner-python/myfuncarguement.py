f8f6a2948c0932d5b43dd57292b790336e4e038b
niniwritescode <nandinibijnor146@gmail.com>
2025-09-02 14:19:20 +0530

    Added average function and tested output

--- beginner-python/myfuncarguement.py
+++ beginner-python/myfuncarguement.py
@@ -1,18 +1,29 @@
-def average(a=9, b=1):
-    print("The average is", (a + b) / 2)
+# def average(a=9, b=1):
+#     print("The average is", (a + b) / 2)
+def average(*numbers):
+    # print(type(numbers))
+    sum = 0
+    for i in numbers:
+       sum = sum + i
+    # print("Average is:", sum/len(numbers))
+    # return 7
+    return sum/len(numbers)
 
 # average(4, 6)
-average(b=9)
-average(a=21)
+# average(b=9)
+# average(a=21)
+c = average(5,7,6,1)
+print(c)
+# def name(fname, mname ="John", lname = "Whatson"):
+#     print("Hello,", fname, mname, lname)
 
-def name(fname, mname ="John", lname = "Whatson"):
-    print("Hello,", fname, mname, lname)
+# name("Amy", "Louise")
 
-name("Amy", "Louise")
+def name(**name):
+    print(type(name))
+    print("Hello,", name["fname"], name["mname"], name["lname"])
 
-
-
-
+name(mname="Buchanan", lname = "Barnes", fname = "James")
 
 
 
