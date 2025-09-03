1ce63dd33d1c82fb86194d2e760397b795ba7e7f
niniwritescode <nandinibijnor146@gmail.com>
2025-09-03 17:44:27 +0530

    added new list and list methods file for practice

---
+++ beginner-python/mylist.py
@@ -0,0 +1,75 @@
+marks = [3, 5, 6, "harry", True]
+# print(marks)
+# print(type(marks))
+# print(marks[0])  
+# print(marks[1])  
+# print(marks[2])  
+# print(marks[3])  
+# print(marks[4])  
+# print(marks[-3]) #Negative index
+# print(marks[len(marks)-3]) #Positive index 
+# print(marks[5-3]) #Positive index
+# print(marks[2]) #Positive index
+
+# if "6" in marks:
+#     print("yes")
+# else:
+#     print("no")
+#Same things applies for strings as well!
+# if "HA" in "harry":
+#     print("yes")
+
+# print(marks)
+# print(marks[1:8]) #Slicing in list
+# print(marks[1:8:2]) #Slicing in list with step or jump index
+
+# lst = [i*i for i in range(4)]
+# print(lst)
+# lst = [i*i for i in range(10) if i%2==0]
+# print(lst)
+
+# names = ["Milon", "Tom", "Jerry", "Harry", "Sally"]
+# namesWith_O = [ item for item in  names if "o" in item]
+# print(namesWith_O)
+
+l = [11, 45, 1, 2, 4, 6, 1, 1, 1]
+print(l)
+# l.append(7)
+#l.sort(reverse=True)
+# l.reverse()
+# print(l.index(1))
+# print(l.count(1))
+# m = l.copy()
+# m[0] = 0
+# l.insert(1, 899)
+m = [900,1000,1100]
+k = l + m
+# print(k)
+# l.extend(m)
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
